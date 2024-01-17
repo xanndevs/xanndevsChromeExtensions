83a5c453b08bfc9d4f728ec4cf3ebba21e02b9e8
xanndevs <canberkcakir04@gmail.com>
2024-01-17 18:12:33 +0300

    Changed the main way of selecting the objects to something more awkward just because of those silly edubiz developers :D Now everything should be fixed and ready to rock'n roll!
    And oh god please dont touch that code again... Ever again!

--- BetterEdubizLessons/button.js
+++ BetterEdubizLessons/button.js
@@ -1,8 +1,6 @@
 x = document.getElementById('changeContent');
-x.addEventListener('click', function() {
-    var newContent = document.getElementById('newContent').value;
-    chrome.runtime.sendMessage({ action: "popupButtonClicked", data: newContent });
-  });
-
-  
-  
+x.addEventListener('click', function () {
+    //var newContent = document.getElementById('newContent').value;
+    //chrome.runtime.sendMessage({ action: "popupButtonClicked", data: newContent });
+    //silly programmers of edubiz thought they could escape from me :D
+});
